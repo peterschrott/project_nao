fbf76c98bd2f339d963b206d20cd6be25668fbcf
x1212 <x1212-x@web.de>
2013-04-18 11:03:45 +0200

    some tests

---
+++ experimental/debug_test/dbg.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main( void )
+{
+
+	int step = 0;
+
+	printf( "step1\n" );
+	step++;
+	printf( "step2\n" );
+	step++;
+	printf( "step3\n" );
+	step++;
+	printf( "step4\n" );
+	step++;
+
+	return;
+}
