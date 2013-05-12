9954facd5f3f13279cf4997ec507c98cbb43ae95
peter <peter@mint-vb.(none)>
2013-05-12 10:12:21 +0200

    renamed recording function, name was not suitable befor

--- experimental/capturevideo/capturevideo.cpp
+++ experimental/capturevideo/capturevideo.cpp
@@ -11,6 +11,7 @@
 #include <alvision/alimage.h>
 #include <alproxies/alvideorecorderproxy.h>
 #include <alvision/alvisiondefinitions.h>
+#include <alproxies/alvideodeviceproxy.h>
 #include <alerror/alerror.h>
 
 // Opencv includes.
@@ -29,7 +30,7 @@ using namespace AL;
 *
 * \param robotIp the IP adress of the robot
 */
-void showImages() {
+void recordMJPG() {
 	/** Create a proxy to ALVideoDevice on the robot.*/
 	ALVideoRecorderProxy alvrp("192.168.0.111", 9559);
 	
@@ -37,9 +38,9 @@ void showImages() {
 	
 	// This records a 320*240 MJPG video at 10 fps.
 	// Note MJPG can't be recorded with a framerate lower than 3 fps.
-	alvrp.setResolution(2);
+	alvrp.setResolution(2);   // 2 = kVGA
 	alvrp.setFrameRate(10);
-	alvrp.setVideoFormat("MJPG");
+	alvrp.setVideoFormat("MJPG");   // "MJPG" (compressed avi)
 	alvrp.startRecording("/home/nao/recordings/cameras", "capture_01");
 	std::cerr << "   started recoding" << std::endl;
 	sleep(15);
@@ -50,11 +51,9 @@ void showImages() {
 	alvrp.stopRecording();
 }
 
-
-
 int main(int argc, char* argv[]) {
 	try {
-		showImages();
+		recordMJPG();
 	} catch (const AL::ALError& e) {
 		std::cerr << "Caught exception " << e.what() << std::endl;
 	}
