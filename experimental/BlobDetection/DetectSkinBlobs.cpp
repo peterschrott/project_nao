85e2dbd54d4998e2b9552c9e34b6a8ec731683b7
peedeeX2 <peter.schrott89@gmail.com>
2013-06-17 11:25:08 +0200

    Initial version of object orientated and modularized approach.

--- experimental/BlobDetection/DetectSkinBlobs.cpp
+++ experimental/BlobDetection/DetectSkinBlobs.cpp
@@ -11,6 +11,8 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+#include <math.h>
+
 
 // Include cvBlobsLib
 #include "BlobResult.h"
@@ -22,9 +24,19 @@ enum handside{LEFT = 0,RIGHT = 1};
 enum gestures{LEFT_FLIP_DOWN = 0,LEFT_FLIP_UP = 1,RIGHT_FLIP_DOWN = 2,RIGHT_FLIP_UP = 3};
 
 int minBlobArea = 200;
+int minMoveDistance = 4;
+int maxMoveDistacne = 50;
 
 using namespace cv;
 using namespace std;
+
+typedef struct {
+    Point centerInit;
+    Point centerPrev;
+    Point centerCurr;
+} Hand;
+
+Hand handRight;
 
 
 CvPoint getCenterPoint(CvRect rect){
@@ -75,11 +87,25 @@ int handleGestures(gestures doGesture){
     }
 }
 
+
+
+int isMoving(Hand h) {
+
+    int movement = sqrt(pow(h.centerCurr.x - h.centerPrev.x, 2) + pow(h.centerCurr.y - h.centerPrev.y , 2)); // pythagoras
+    std::cout << "               movement: "<< movement <<endl;
+    if(movement > minMoveDistance && movement < maxMoveDistacne) {
+        // the point is within the movement tolerance
+        // moves more then minMoveDestinace and less then MaxMoveDistance
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
 int main(int argc, char *argv[])
 {
-
-
-    CvCapture* capture = cvCreateFileCapture( "recording_03.avi");
+    CvCapture* capture = cvCreateFileCapture( "recording_01.avi");
 
 
 
@@ -87,20 +113,18 @@ int main(int argc, char *argv[])
     handOrientation rightOrientationCur = NONE, leftOrientationCur = NONE;
 
 
-	cvNamedWindow("Input Image", CV_WINDOW_AUTOSIZE);
-	cvNamedWindow("Skin Pixels", CV_WINDOW_AUTOSIZE);
+	//cvNamedWindow("Input Image", CV_WINDOW_AUTOSIZE);
+	//cvNamedWindow("Skin Pixels", CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("Skin Blobs", CV_WINDOW_AUTOSIZE);
 
     while(1){
-        Mat imageBGR =  cvQueryFrame(capture);
+        Mat imageBGR = cvQueryFrame(capture);
         if(imageBGR.empty())break;
-        imshow("Input Image", imageBGR);
+        //imshow("Input Image", imageBGR);
 
         // Convert the image to HSV colors.
         Mat imageHSV = Mat(imageBGR.size(), CV_8UC3);	// Full HSV color image.
         cvtColor(imageBGR, imageHSV, CV_BGR2HSV);				// Convert from a BGR to an HSV image.
-
-
 
         std::vector<Mat> channels(3);
         split(imageHSV, channels);
@@ -111,20 +135,20 @@ int main(int argc, char *argv[])
 
 
         // Detect which pixels in each of the H, S and V channels are probably skin pixels.
-        threshold(planeH, planeH, 150, UCHAR_MAX, CV_THRESH_BINARY_INV);//18
-        threshold(planeS, planeS, 60, UCHAR_MAX, CV_THRESH_BINARY);//50
-        threshold(planeV, planeV, 170, UCHAR_MAX, CV_THRESH_BINARY);//80
+        threshold(channels[0], channels[0], 150, UCHAR_MAX, CV_THRESH_BINARY_INV);//18
+        threshold(channels[1], channels[1], 60, UCHAR_MAX, CV_THRESH_BINARY);//50
+        threshold(channels[2], channels[2], 170, UCHAR_MAX, CV_THRESH_BINARY);//80
 
 
         // Combine all 3 thresholded color components, so that an output pixel will only
         // be white if the H, S and V pixels were also white.
         Mat imageSkinPixels = Mat( imageBGR.size(), CV_8UC3);	// Greyscale output image.
-        bitwise_and(planeH, planeS, imageSkinPixels);				// imageSkin = H {BITWISE_AND} S.
-        bitwise_and(imageSkinPixels, planeV, imageSkinPixels);	// imageSkin = H {BITWISE_AND} S {BITWISE_AND} V.
+        bitwise_and(channels[0], channels[1], imageSkinPixels);				// imageSkin = H {BITWISE_AND} S.
+        bitwise_and(imageSkinPixels, channels[2], imageSkinPixels);	// imageSkin = H {BITWISE_AND} S {BITWISE_AND} V.
 
         // Show the output image on the screen.
 
-        imshow("Skin Pixels", imageSkinPixels);
+        //imshow("Skin Pixels", imageSkinPixels);
 
 
         IplImage ipl_imageSkinPixels = imageSkinPixels;
@@ -158,13 +182,11 @@ int main(int argc, char *argv[])
 
         //Gestures
 
-        std::cout << "Number of Blobs: "<< blobs.GetNumBlobs() <<endl;
-
-
+        //std::cout << "Number of Blobs: "<< blobs.GetNumBlobs() <<endl;
 
         if(blobs.GetNumBlobs() == 0){
             //picture empty
-        }else if(blobs.GetNumBlobs() == 1){
+        }else if(blobs.GetNumBlobs() == 1) {
             //head detected
         }else if(blobs.GetNumBlobs() == 2 || blobs.GetNumBlobs() == 3){
             //head + one hand || head + two hands
@@ -192,6 +214,7 @@ int main(int argc, char *argv[])
                     indexHandLeft = 1;
                     indexHandRight = -1;
                 }else{
+                    // right hand
                     indexHandLeft = -1;
                     indexHandRight = 1;
                 }
@@ -223,13 +246,36 @@ int main(int argc, char *argv[])
                 }
             }
 
+            // follow the right hand
+            if(indexHandRight > 0) {
+                //std::cout << "right hand deteced" <<endl;
+                if(isMoving(handRight)) {
+                    std::cout << "hand moving" <<endl;
+                    handRight.centerPrev = handRight.centerCurr;
+                    handRight.centerCurr = getCenterPoint(rect[indexHandRight]);
+                } else {
+                    std::cout << "hand not moving" <<endl;
+                    if(handRight.centerInit.y != 0 && abs(handRight.centerInit.y - handRight.centerCurr.y) > 20) {
+                        if(handRight.centerInit.y < handRight.centerCurr.y) {
+                            // hand moved down
+                            std::cout << "                           hand moved down" <<endl;
+                        } else {
+                            // hand moved up
+                            std::cout << "                           hand moved up" <<endl;
+                        }
+                    }
+                    handRight.centerInit = getCenterPoint(rect[indexHandRight]);
+                    handRight.centerPrev = handRight.centerCurr;
+                    handRight.centerCurr = getCenterPoint(rect[indexHandRight]);
+                }
+            }
 
             //Get Orientations from Hand rects
             leftOrientationCur = (indexHandLeft != -1)?getOrientationOfRect(rect[indexHandLeft]):NONE;
             rightOrientationCur = (indexHandRight != -1)?getOrientationOfRect(rect[indexHandRight]):NONE;
 
             //Check Change of Left hand
-            switch(detectHandStateChange(leftOrientationLast, leftOrientationCur)){
+            /*switch(detectHandStateChange(leftOrientationLast, leftOrientationCur)){
                 case PORTRAIT_TO_LANDSCAPE:
                     handleGestures(LEFT_FLIP_DOWN);
                     break;
@@ -252,19 +298,13 @@ int main(int argc, char *argv[])
                 case NOCHANGE:
                 default:
                     break;
-            }
+            }*/
 
 
         }else if(blobs.GetNumBlobs() > 3){
             //too much information
             cout<<"too much information"<<endl;
         }
-
-
-
-
-
-
 
         leftOrientationLast = leftOrientationCur;
         rightOrientationLast = rightOrientationCur;
