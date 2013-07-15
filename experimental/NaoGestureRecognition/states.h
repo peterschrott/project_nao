827fd1163089a2c9e89f52f868f0c9f163befafe
peedeeX21 <peter.schrott89@gmail.com>
2013-07-15 21:32:32 +0200

    the current version of the geastur recognition is transfered to the source folder (../src/...)

--- experimental/NaoGestureRecognition/states.h
+++
@@ -1,8 +0,0 @@
-#ifndef STATES_H
-#define STATES_H
-
-
-typedef enum {LEFT_UP_RIGHT_DOWN = 2,LEFT_DOWN_RIGHT_UP = 1, BOTH_UP = 3, BOTH_DOWN = 0} HandStatus;
-typedef enum {LEFT_FLIP_DOWN = 0,LEFT_FLIP_UP = 1,RIGHT_FLIP_DOWN = 2,RIGHT_FLIP_UP = 3} Gesture;
-
-#endif
